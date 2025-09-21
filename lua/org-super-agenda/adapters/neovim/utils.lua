2064358951c5695340c14f70dde38ede173eb059
Mohamed Hamidi <mohamed@hamidi.dev>
2025-09-21 13:39:01 +0200

    chore: adds 'undo' to 'show help'

--- lua/org-super-agenda/adapters/neovim/utils.lua
+++ lua/org-super-agenda/adapters/neovim/utils.lua
@@ -51,6 +51,7 @@ function U.show_help()
     fmt('<CR>', 'Open headline'),
     fmt(km.reload, 'Reload agenda'),
     fmt(km.cycle_todo, 'Cycle TODO-States'),
+    fmt(km.undo, 'Undo'),
     fmt(km.toggle_other, 'Toggle "Other" group'),
     fmt(km.toggle_duplicates, 'Toggle duplicates'),
     fmt(km.cycle_view, 'Switch view (classic/compact)'),
